bae5c1aa810cc5510193450d7abdfe286b7c7898
Guido <guido.bassi@hotmail.com>
2023-04-07 22:48:12 -0300

    max puntuacion

--- script.js
+++ script.js
@@ -10,26 +10,63 @@ var questions = [
 	{
 		question: "¿Los seres humanos tienen 3 pulmones?",
 		answer: false
+	},
+	{
+		question: "¿La luna tiene luz propia?",
+		answer: false
+	},
+	{
+		question: "¿El color del caballo blanco de Napoleón era blanco?",
+		answer: true
 	}
 ];
 
+var shuffledQuestions = shuffle(questions);
 var currentQuestion = 0;
+var score = 0;
+var highScore = localStorage.getItem("highScore") || 0;
+
+function shuffle(array) {
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
 
 function checkAnswer(userAnswer) {
 	var result = document.getElementById("result");
 
-	if (userAnswer === questions[currentQuestion].answer) {
+	if (userAnswer === shuffledQuestions[currentQuestion].answer) {
 		result.innerHTML = "¡Correcto!";
+		score++;
 	} else {
-		result.innerHTML = "Incorrecto. La respuesta correcta es " + (questions[currentQuestion].answer ? "verdadero" : "falso") + ".";
+		result.innerHTML = "Incorrecto. La respuesta correcta es " + (shuffledQuestions[currentQuestion].answer ? "verdadero" : "falso") + ".";
 	}
 
 	currentQuestion++;
 
-	if (currentQuestion < questions.length) {
-		document.getElementById("question").innerHTML = questions[currentQuestion].question;
+	if (currentQuestion < shuffledQuestions.length) {
+		document.getElementById("question").innerHTML = shuffledQuestions[currentQuestion].question;
 	} else {
 		document.getElementById("question").innerHTML = "¡Fin del juego!";
 		document.getElementsByTagName("div")[0].style.display = "none";
+		result.innerHTML += " Obtuviste " + score + " puntos.";
+
+		if (score > highScore) {
+			highScore = score;
+			localStorage.setItem("highScore", highScore);
+			result.innerHTML += " ¡Nueva puntuación máxima!";
+		} else {
+			result.innerHTML += " Puntuación máxima: " + highScore;
+		}
 	}
 }
